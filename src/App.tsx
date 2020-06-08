f8f6319a2107d2dfc7255f89a6e8a2464e74f47a
Corvax <vlvete1990@yandex.ru>
2020-06-08 12:27:21 +0300

    adding folders

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo192'
+
+// root app
+const App = () => {
+  return (
+    <div className='wrapper constent'>
+        Starter React
+      <br/>
+      <div>Test</div>
+      <img src={logo} alt="" srcSet=""/>
+    </div>
+  )
+}
+
+export default App
